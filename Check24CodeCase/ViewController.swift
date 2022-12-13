180d8fc307d4a9a77db908428612d1d44de40311
Doğukaan Kılıçarslan <dogukaan.kilicarslan@hepsiburada.com>
2022-12-13 10:52:51 +0300

    Initial Commit

---
+++ Check24CodeCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Check24CodeCase
+//
+//  Created by Doğukaan Kılıçarslan on 13.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
